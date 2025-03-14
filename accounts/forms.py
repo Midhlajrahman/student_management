57efc9a81366bfa1111928e67aa764c93cfbcb8c
Shabeeb942 <shabeebsha942@gmail.com>
2025-03-14 21:13:31 +0530

    register with usertype

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import User
+from web.models import Department,Semester
 from registration.forms import RegistrationForm
 
 class CustomRegistrationForm(RegistrationForm):
@@ -7,14 +8,45 @@ class CustomRegistrationForm(RegistrationForm):
         choices=[("student", "Student"), ("faculty", "Faculty")],
         widget=forms.Select(attrs={"class": "form-control"})
     )
-    full_name = forms.CharField(required=True, widget=forms.TextInput(attrs={"class": "form-control"}))
-    id_number = forms.CharField(required=False, widget=forms.TextInput(attrs={"class": "form-control"}))
-    register_number = forms.CharField(required=False, widget=forms.TextInput(attrs={"class": "form-control"}))
+    full_name = forms.CharField(
+        required=True,
+        widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Full Name"})
+    )
+    id_number = forms.CharField(
+        required=False,
+        widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "ID Number"})
+    )
+    register_number = forms.CharField(
+        required=False,
+        widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Register Number"})
+    )
     semester = forms.ModelChoiceField(
-        queryset=User.objects.all(), required=False, widget=forms.Select(attrs={"class": "form-control"})
+        queryset=Semester.objects.all(),
+        required=False,
+        empty_label="Select Semester",  # Adds a placeholder-like option
+        widget=forms.Select(attrs={"class": "form-control"})
     )
     department = forms.ModelChoiceField(
-        queryset=User.objects.all(), required=True, widget=forms.Select(attrs={"class": "form-control"})
+        queryset=Department.objects.all(),
+        required=True,
+        empty_label="Select Department",  # Adds a placeholder-like option
+        widget=forms.Select(attrs={"class": "form-control"})
+    )
+    username = forms.CharField(
+        required=True,
+        widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Username"})
+    )
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(attrs={"class": "form-control", "placeholder": "Email"})
+    )
+    password1 = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={"class": "form-control", "placeholder": "Password"})
+    )
+    password2 = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={"class": "form-control", "placeholder": "Confirm Password"})
     )
 
     class Meta:
